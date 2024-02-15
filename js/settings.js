c725821aaa7e9b7feb50372befb054b718cd8420
markGambrelPhila <118294876+markGambrelPhila@users.noreply.github.com>
2024-02-15 15:39:45 -0500

    Update council districts

--- js/settings.js
+++ js/settings.js
@@ -14,7 +14,7 @@ define({
     "parcelsUrl": "//services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/PWD_PARCELS/FeatureServer/0",
     "dorParcelsUrl": "//services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/DOR_Parcel/FeatureServer/0",
     "rcoUrl": "//services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Zoning_RCO/FeatureServer/0",
-    "councilUrl": "//services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Council_Districts_2016/FeatureServer/0",
+    "councilUrl": "//services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Council_Districts_2024/FeatureServer/0",
     "cartoOpaPropertiesUrl": "https://phl.carto.com:443/api/v2/sql",
     // FIELDS
     "rcoFields": ["ORGANIZATION_NAME", "PRIMARY_EMAIL", "ORGANIZATION_ADDRESS", "PRIMARY_NAME", "PRIMARY_ADDRESS", "PRIMARY_PHONE"],
